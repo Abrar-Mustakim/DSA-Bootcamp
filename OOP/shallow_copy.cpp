0f481c8d0fd009894de2a30bea09f40528d76f26
Abrar-Mustakim <sm.abrar.mustakim@g.bracu.ac.bd>
2025-06-29 01:07:55 +0600

    Shallow Copy vs DeepCopy

---
+++ OOP/shallow_copy.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Student {
+public:
+    string name; 
+    double* cgpaPtr;
+
+    Student(string name, double cgpa){
+        this -> name = name;
+        //allocate new dynamic memory to pointer
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+
+    }
+
+    //DeepCopy
+    Student(Student &newObj){
+        this->name=newObj.name;
+        //this->cgpaPtr=newObj.cgpaPtr;
+        cgpaPtr = new double; //create new memory allocation for the Copy Also
+        *cgpaPtr = *(newObj.cgpaPtr);
+    }
+
+    void getInfo(){
+        cout << "Student Name: " << name << " and his CGPA: " << *cgpaPtr << endl;
+    }
+
+};
+
+
+int main(){
+    Student s1("abc", 3.5);
+    s1.getInfo();
+
+    Student s2(s1);
+    s2.name="def";
+    s2.getInfo();
+
+    *(s2.cgpaPtr) = 3.7;
+    s1.getInfo();
+    s2.getInfo();
+    return 0;
+}
